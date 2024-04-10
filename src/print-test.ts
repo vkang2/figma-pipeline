777d38741f20526ba6a30c1ebf7fce4ecc8be027
vkang2 <69218529+vkang2@users.noreply.github.com>
2024-04-10 09:23:15 -0700

    read file

--- src/print-test.ts
+++ src/print-test.ts
@@ -1,5 +1,9 @@
+import { readFileSync } from "fs";
 async function main() {
+  const file = readFileSync('fetch-api-data-action/figma.json', 'utf-8');
+
   console.log("Hello, world!");
+  console.log(file)
 }
 
 main();
