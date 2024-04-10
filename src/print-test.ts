f1808dbc7cd61aaba5843132565813167342afc1
vkang2 <69218529+vkang2@users.noreply.github.com>
2024-04-10 09:32:09 -0700

    write to a file

--- src/print-test.ts
+++ src/print-test.ts
@@ -1,6 +1,9 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
+
 async function main() {
   const file = readFileSync('fetch-api-data-action/figma.json', 'utf-8');
+
+  writeFileSync('fetch-api-data-action/figma_copy.json', 'utf-8');
 
   console.log("Hello, world!");
   console.log(file)
