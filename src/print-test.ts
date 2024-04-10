da54f7e4df2a9c6a3c9fa93b06c54f2b1297b136
vkang2 <69218529+vkang2@users.noreply.github.com>
2024-04-10 09:39:55 -0700

    fix format and remove file

--- src/print-test.ts
+++ src/print-test.ts
@@ -3,7 +3,7 @@ import { readFileSync, writeFileSync } from "fs";
 async function main() {
   const file = readFileSync('fetch-api-data-action/figma.json', 'utf-8');
 
-  writeFileSync('fetch-api-data-action/figma_copy.json', JSON.stringify(file, null, 2))
+  writeFileSync('fetch-api-data-action/figma_copy.json', file, 'utf-8')
 
   console.log("Hello, world!");
   console.log(file)
