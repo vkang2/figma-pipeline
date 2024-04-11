5b8bfffbafac8f94f0d79712edaa9e191861b93a
vkang2 <69218529+vkang2@users.noreply.github.com>
2024-04-11 16:52:56 -0700

    clean up file

--- src/convert-figma-variables.ts
+++ src/convert-figma-variables.ts
@@ -2,62 +2,6 @@ import { GetLocalVariablesResponse, LocalVariable } from "@figma/rest-api-spec";
 import { RGB, RGBA } from "@figma/rest-api-spec";
 
 /**
- * This file defines what design tokens and design token files look like in the codebase.
- *
- * Tokens are distinct from variables, in that a [token](https://tr.designtokens.org/format/#design-token)
- * is a name/value pair (with other properties), while a variable in Figma stores multiple values,
- * one for each mode.
- */
-
-import { VariableCodeSyntax, VariableScope } from "@figma/rest-api-spec";
-
-export interface Token {
-  /**
-   * The [type](https://tr.designtokens.org/format/#type-0) of the token.
-   *
-   * We allow `string` and `boolean` types in addition to the draft W3C spec's `color` and `number` types
-   * to align with the resolved types for Figma variables.
-   */
-  $type: "color" | "number" | "string" | "boolean";
-  $value: string | number | boolean;
-  $description?: string;
-  $extensions?: {
-    /**
-     * The `com.figma` namespace sto`res Figma-specific variable properties
-     */
-    "com.figma"?: {
-      hiddenFromPublishing?: boolean;
-      scopes?: VariableScope[];
-      codeSyntax?: VariableCodeSyntax;
-    };
-  };
-}
-
-export type TokenOrTokenGroup =
-  | Token
-  | ({
-      [tokenName: string]: Token;
-    } & { $type?: never; $value?: never });
-
-/**
- * Defines what we expect a Design Tokens file to look like in the codebase.
- *
- * This format mostly adheres to the [draft W3C spec for Design Tokens](https://tr.designtokens.org/format/)
- * as of its most recent 24 July 2023 revision except for the $type property, for which
- * we allow `string` and `boolean` values in addition to the spec's `color` and `number` values.
- * We need to support `string` and `boolean` types to align with the resolved types for Figma variables.
- *
- * Additionally, we expect each tokens file to define tokens for a single variable collection and mode.
- * There currently isn't a way to represent modes or themes in the W3C community group design token specification.
- * Once the spec resolves how it wants to treat/handle modes, this code will be updated to reflect the new standard.
- *
- * Follow this discussion for updates: https://github.com/design-tokens/community-group/issues/210
- */
-export type TokensFile = {
-  [key: string]: TokenOrTokenGroup;
-};
-
-/**
  * Compares two colors for approximate equality since converting between Figma RGBA objects (from 0 -> 1) and
  * hex colors can result in slight differences.
  */
@@ -152,17 +96,17 @@ function getValueFromVariable(
     name: string;
   },
   localVariables: { [id: string]: LocalVariable },
-  variableValues: { [key: string]: any },
+  variableValues: { [key: string]: any }
 ) {
   const value = variable.valuesByMode[mode.modeId];
   if (typeof value === "object") {
     if ("type" in value && value.type === "VARIABLE_ALIAS") {
-      console.log(mode)
+      console.log(mode);
 
       const aliasVariable = localVariables[value.id];
       const aliasVariableName = getCSSVariableName(aliasVariable, mode);
-      
-      console.log(aliasVariableName)
+
+      console.log(aliasVariableName);
       const aliasedVariableValue = variableValues[aliasVariableName];
       return aliasedVariableValue;
     } else if ("r" in value) {
